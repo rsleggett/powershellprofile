87b25a070b7a0d8f6d8ef598de927aee877942c8
Rob <r.leggett@building-blocks.com>
2016-07-20 07:30:50 +0100

    update profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,22 @@ function tail($file)
     Get-Content $file -Wait -Tail 100 
 }
 
-function setenv($key, $value, $space) 
+function setenvmachine($key, $value)
+{
+    setenv($key, $value, [EnvironmentVariableTarget]::Machine)
+}
+
+function setenvuser($key, $value)
+{
+    setenv($key, $value,  [EnvironmentVariableTarget]::User)
+}
+
+function setenv($key, $value, $level) 
 { 
-    [Environment]::SetEnvironmentVariable($key, $value, $space) 
+    [Environment]::SetEnvironmentVariable($key, $value, $level) 
+}
+
+function addtopath($path)
+{
+    [Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $path, [EnvironmentVariableTarget]::Machine)
 }
